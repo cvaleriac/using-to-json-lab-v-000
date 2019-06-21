6591ac489e4447360f18bf95b9321863a7105aff
valeria campilongo <cvaleriac@hotmail.it>
2019-06-21 17:19:42 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,7 @@ class ProductsController < ApplicationController
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:id, :name, :description, :inventory, :price])}
   end
+end
 
   def data
     product = Product.find(params[:id])
